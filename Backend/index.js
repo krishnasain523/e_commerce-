960ef0f62131eea22a703cf1ab60ed78d3931746
Samarth MAlhotra <samarthmalhotra4200@gmail.com>
2026-04-04 23:46:55 +0530

    add..

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,30 @@
-require("dotenv").config();
-const express=require("express");
-const app= express();
-const Port=3000;
-// npm i cookie-parser cors passport express-json already installed
-app.listen(Port,()=>{
-    console.log(`The server is running on port ${Port}`);
-})
+//install dependencies
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+//Dotenv
+dotenv.config();
+//Server
+const server = express();
+//Connect backend with frontend
+server.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
+//PORT
+const PORT = process.env.PORT;
 
+server.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("helo krishna");
-})
+//It convert urlencoded data in req body
+server.use(express.urlencoded({ extended: true }));
+
+// Test Route
+server.get("/", (req, res) => {
+  res.send("Server is running");
+});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
