80270a9ee950700f2e8f508d529af581ead6b1b5
Harsh <harshdhiman7817@gmail.com>
2026-04-05 23:34:47 +0530

    add frontend

---
+++ Frontend/src/pages/Admin/AddProduct.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddProduct = () => {
+  return (
+  <div className='flex justify-center align-middle'>
+    <form action="" className='border w-[60%]'>
+    <input type="text" />
+    </form>
+  </div>
+  )
+}
+
+export default AddProduct
